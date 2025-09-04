a8858e6a738ec8058fce3a306fdbae042b565490
Kasper Vikström <kasper.vik@hotmail.com>
2025-09-04 10:22:41 +0200

    Back to homepage button

--- app/wines/page.tsx
+++ app/wines/page.tsx
@@ -1,10 +1,16 @@
 import WineList from "@/app/ui/wine-list";
 import { prisma } from "@/prisma/db";
+import Link from "next/link";
 
 export default async function WinesPage() {
   const wines = await prisma.wine.findMany({ orderBy: { createdAt: "desc" } });
+
   return (
     <main>
+      <Link href="/" className="backButton">
+        ← gå tillbaka
+      </Link>
+
       <WineList defaultWines={wines} />
     </main>
   );
