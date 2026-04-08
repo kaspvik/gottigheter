7cdfd7eb52c3bcf06caad98096c531b8eb5142ff
Kasper Vikström <kasper.vik@hotmail.com>
2026-04-08 14:36:35 +0200

    fix: buildfel

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,10 +2,7 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  datasource: {
-    url: process.env.DATABASE_URL!,
-  },
   migrations: {
-    seed: `tsx prisma/seed`,
+    seed: "tsx prisma/seed",
   },
 });
